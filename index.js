f17cf01aba4c9814a11f24fb672c98e0ab9e463c
Alireja-khan <alirejakhan18@gmail.com>
2025-10-24 00:25:17 +0600

    add another domain

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(cors({
     "http://localhost:5173", 
     "http://localhost:3000", 
     "https://utter-waste.surge.sh",
-    "https://flourishing-pony-67c650.netlify.app"
+    "https://flourishing-pony-67c650.netlify.app",
+    "https://food-cirrcle.netlify.app/",
   ],
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
   credentials: true
@@ -69,7 +70,8 @@ const io = new Server(server, {
       "http://localhost:3000", 
       "https://utter-waste.surge.sh",
       "https://food-circle-server-9zagiegfa-alireja-khans-projects.vercel.app",
-      "https://flourishing-pony-67c650.netlify.app" 
+      "https://flourishing-pony-67c650.netlify.app",
+      "https://food-cirrcle.netlify.app/",
     ],
     methods: ["GET", "POST"],
     credentials: true
